0b5569e7ee6519b49624d2245a079e7434091695
Joe Karow <hello@joekarow.dev>
2022-04-30 18:34:27 -0400

    configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,125 @@
-module.exports = {
-  siteMetadata: {
-    title: `html5up-landed-gatsby`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-sass", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-mdx", {
+const cmsJsonSettings = require('./src/data/settings/settings.json');
+
+const { siteMetadata } = cmsJsonSettings;
+// const siteMetadata = {
+//   title: 'html5up-landed-gatsby',
+//   siteUrl: 'https://www.yourdomain.tld',
+// };
+const sourceFilesystemList = [
+  // gatsby images -- *keep first*
+  {
     resolve: 'gatsby-source-filesystem',
     options: {
-      "name": "pages",
-      "path": "./src/pages/"
+      name: 'uploads',
+      path: `${__dirname}/static/img`,
     },
-    __key: "pages"
-  }]
+    __key: 'uploads',
+  },
+  // pages
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      name: 'pages',
+      path: `${__dirname}/src/pages`,
+    },
+    __key: 'pages',
+  },
+  // cms data files
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      name: 'data',
+      path: `${__dirname}/src/data`,
+    },
+  },
+];
+const pluginImageSharp = {
+  resolve: 'gatsby-plugin-sharp',
+  options: {
+    defaults: {
+      formats: ['auto', 'webp'],
+      placeholder: 'blurred',
+      quality: 50,
+      breakpoints: [750, 1080, 1366, 1920],
+      backgroundColor: 'transparent',
+      tracedSVGOptions: {},
+      blurredOptions: {},
+      jpgOptions: {},
+      pngOptions: {},
+      webpOptions: {},
+      avifOptions: {},
+    },
+  },
+};
+const pluginMdx = {
+  resolve: 'gatsby-plugin-mdx',
+  options: {
+    extensions: ['.mdx', '.md'],
+    gatsbyRemarkPlugins: [
+      {
+        resolve: 'gatsby-remark-relative-images',
+        options: {
+          name: 'uploads',
+        },
+      },
+      {
+        resolve: 'gatsby-remark-images',
+        options: {
+          // It's important to specify the maxWidth (in pixels) of
+          // the content container as this plugin uses this as the
+          // base for generating different widths of each image.
+          maxWidth: 2048,
+        },
+      },
+      {
+        resolve: 'gatsby-remark-copy-linked-files',
+        options: {
+          destinationDir: 'static',
+        },
+      },
+      {
+        resolve: 'gatsby-remark-external-links',
+      },
+    ],
+  },
+};
+const pluginNetlifyCMS = {
+  resolve: 'gatsby-plugin-netlify-cms',
+  options: {
+    manualInit: true,
+    modulePath: `${__dirname}/src/cms/index.js`,
+  },
+};
+const pluginCSS = {
+  resolve: 'gatsby-plugin-sass',
+  options: {
+    // useResolveUrlLoader: true,
+    postCssPlugins: [],
+    // sassOptions: {
+    //   indentedSyntax: false,
+    //   includePaths: ['./src/css', './src/css/libs'],
+    // },
+  },
+};
+
+const plugins = [
+  'gatsby-plugin-react-helmet',
+  'gatsby-plugin-sitemap',
+  'gatsby-plugin-image',
+'gatsby-plugin-mdx',
+  'gatsby-plugin-postcss',
+  'gatsby-transformer-json',
+  'gatsby-transformer-sharp',
+  pluginImageSharp,
+  pluginCSS,
+  pluginNetlifyCMS,
+
+  ...sourceFilesystemList,
+  //keep this last
+  'gatsby-plugin-netlify',
+];
+
+module.exports = {
+  siteMetadata: siteMetadata,
+  plugins: plugins,
 };
---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    screens: {
+      sm: '481px',
+      md: '737px',
+      lg: '981px',
+      xl: '1281px',
+    },
+    aspectRatio: {
+      auto: 'auto',
+      square: '1 / 1',
+      video: '16 / 9',
+      1: '1',
+      2: '2',
+      3: '3',
+      4: '4',
+      5: '5',
+      6: '6',
+      7: '7',
+      8: '8',
+      9: '9',
+      10: '10',
+      11: '11',
+      12: '12',
+      13: '13',
+      14: '14',
+      15: '15',
+      16: '16',
+    },
+  },
+  corePlugins: {
+  },
+  variants: {},
+  // plugins: [
+  //   require('@tailwindcss/typography'),
+  //   require('@tailwindcss/aspect-ratio'),
+  // ],
+};
