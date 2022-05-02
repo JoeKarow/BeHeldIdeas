58dd39ac2ec9a74685d53ae11f8e8f73b81d63fc
Joe Karow <hello@joekarow.dev>
2022-05-02 12:26:51 -0400

    separate page content cms configs

--- src/cms/config/collections/pageContent/index.js
+++ src/cms/config/collections/pageContent/index.js
@@ -1,37 +1,6 @@
-import {
-  imageField,
-  mdEditor,
-  mdButtons,
-  objectField,
-  stringField
-} from '../../fields'
-import { collectionDefaults, pageDefaults } from '../../patterns'
+import { collectionDefaults } from '../../patterns'
+import { mainPage } from './mainPage'
 
-console.log(mdEditor('Blurb', 'content'))
-const heroSectionFields = [
-  stringField('Main heading', 'heading', true),
-  stringField('Subheading', 'subheading', true),
-  imageField('Main Image', 'image', '../../static/img', false, true),
-  stringField('Image Alt Text', 'imgAlt', true)
-]
-const aboutSectionFields = [
-  imageField('Featured Image', 'image', '../../static/img', false, true),
-  stringField('Image Alt Text', 'imgAlt', true),
-  stringField('Main heading', 'heading', true),
-  stringField('Subheading', 'subheading', true),
-  mdEditor('Blurb', 'content')
-]
-
-const mainPage = {
-  label: 'Main Page',
-  name: 'main',
-  file: 'src/pages/index.mdx',
-  fields: [
-    ...pageDefaults('index-page'),
-    objectField('Top Section', 'hero', heroSectionFields, true),
-    objectField('About Section', 'about', aboutSectionFields, true)
-  ]
-}
 const pageContentCollection = {
   ...collectionDefaults('Page Content', 'pagecontent'),
   extension: 'mdx',
