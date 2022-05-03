f82dfa02d28de2bf6d1ab0630ee531b4d1081531
Joe Karow <hello@joekarow.dev>
2022-05-03 11:44:08 -0400

    netlify pnpm config

--- .netlify/plugins/netlify-plugin-pnpm/index.js
+++ .netlify/plugins/netlify-plugin-pnpm/index.js
@@ -1,9 +1,12 @@
 module.exports = {
   onPreBuild: async ({ utils: { build, run } }) => {
     try {
+      console.log('Attempting pnpm installation')
       await run.command('npm install -g pnpm')
+      console.log('Installing packages with pnpm')
       await run.command('pnpm install --frozen-lockfile=false')
     } catch (error) {
+      console.log('Ugh - something went wrong.')
       return build.failBuild(error)
     }
   }
