8b61ba6c6929cac7e21f7e7245229f15899e4fbe
Joe Karow <hello@joekarow.dev>
2022-05-03 14:42:02 -0400

    separate out timeline generation function

---
+++ workers/timelineGenerator.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { TimelineCard } from '../src/components/TimelineCard'
+
+const timelineGenerator = items =>
+  items.map((item, index) => {
+    const itemKey = (index + 1).toString().padStart(3, '0').toString()
+    const cardOptions = {
+      dateInnerStyle: {
+        background: item.dates.background,
+        color: item.dates.color
+      }
+    }
+    return (
+      <TimelineCard
+        itemOptions={cardOptions}
+        dateText={item.dates.text}
+        heading={item.heading}
+        subheading={item.subheading}
+        body={item.subheading}
+      />
+    )
+  })
+
+export default timelineGenerator
