acea7ca1ca241df5a075bf5582c370b11e1ceebc
Joe Karow <hello@joekarow.dev>
2022-05-04 01:21:07 -0400

    merge html5up theme items in to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,52 @@ module.exports = {
       lg: '981px',
       xl: '1281px'
     },
+    // extend: {
+    // HTML5up Theme Colors
+    colors: {
+      bg: '#1c1d26',
+      'bg-transparent': 'rgba(23,24,32,0.95)',
+      'fg-bold': '#ffffff',
+      fg: 'rgba(255,255,255,0.75)',
+      'fg-light': 'rgba(255,255,255,0.5)',
+      'fg-lighter': 'rgba(255,255,255,0.15)',
+      border: 'rgba(255,255,255,0.3)',
+      'border-bg': 'rgba(255,255,255,0.075)',
+      border2: 'rgba(255,255,255,0.5)',
+      'border2-bg': 'rgba(255,255,255,0.25)',
+      accent1: '#e44c65',
+      accent2: '#272833',
+      'accent2-transparent': 'rgba(39,40,51,0.965)',
+      accent3: '#5480f1',
+      accent4: '#39c088'
+    },
+    backgroundColor: {
+      bg: '#1c1d26',
+      'bg-transparent': 'rgba(23,24,32,0.95)',
+      'border-bg': 'rgba(255,255,255,0.075)',
+      'border2-bg': 'rgba(255,255,255,0.25)'
+    },
+    borderColor: {
+      border: 'rgba(255,255,255,0.3)',
+      border2: 'rgba(255,255,255,0.5)'
+    },
+    accentColor: {
+      accent1: '#e44c65',
+      accent2: '#272833',
+      'accent2-transparent': 'rgba(39,40,51,0.965)',
+      accent3: '#5480f1',
+      accent4: '#39c088'
+    },
+    divideColor: ({ theme }) => ({
+      ...theme('borderColor')
+    }),
+    textColor: {
+      'fg-bold': '#ffffff',
+      fg: 'rgba(255,255,255,0.75)',
+      'fg-light': 'rgba(255,255,255,0.5)',
+      'fg-lighter': 'rgba(255,255,255,0.15)'
+    },
+    // },
     aspectRatio: {
       auto: 'auto',
       square: '1 / 1',
@@ -32,7 +78,7 @@ module.exports = {
   corePlugins: {},
   variants: {},
   plugins: [
-    require('@tailwindcss/typography')
-    //   require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio')
   ]
 }
