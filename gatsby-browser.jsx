2be6473d8efcf2a97ac5eb946d0ff6cdb0f7dec9
Joe Karow <hello@joekarow.dev>
2022-05-04 23:37:38 -0400

    migrate to jsx

--- gatsby-browser.jsx
+++ gatsby-browser.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import './src/css/tailwind.scss'
 import './src/css/main.scss'
+import './src/css/tailwind.scss'
 
 import { BreakpointProvider, setDefaultBreakpoints } from 'react-socks'
 import Layout from './src/components/Layout'
+import { MDXProvider } from '@mdx-js/react'
+import { SocialLinks } from './src/components'
 
+// React-Socks Breakpoints
 setDefaultBreakpoints([
   { sm: '481px' },
   { md: '737px' },
@@ -12,8 +15,17 @@ setDefaultBreakpoints([
   { xl: '1281px' }
 ])
 
+// MDX Components
+const mdxShortcodes = {
+  SocialLinks
+}
+
 export const wrapRootElement = ({ element }) => {
-  return <BreakpointProvider>{element}</BreakpointProvider>
+  return (
+    <MDXProvider components={mdxShortcodes}>
+      <BreakpointProvider>{element}</BreakpointProvider>)
+    </MDXProvider>
+  )
 }
 
 export const wrapPageElement = ({ element, props }) => {
