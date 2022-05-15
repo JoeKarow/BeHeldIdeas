69209cbb90d439a95ad1ed1e842729b79b3678c3
Joe Karow <hello@joekarow.dev>
2022-05-15 14:51:10 -0400

    separate css to module

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import settingsJSON from '../data/settings/settings.json';
+import React from 'react'
+import { siteMetadata } from '../data/settings/settings.json'
+import * as styles from './copyright.module.scss'
 
 const Copyright = () => {
   return (
-    <ul className='copyright'>
-      <li>&copy; {settingsJSON.author}. All rights reserved.</li>
+    <ul className={styles.copyright}>
+      <li>&copy; {siteMetadata.author}. All rights reserved.</li>
       <li>
-        Development: <a href='https://joekarow.dev'>JoeKarow.dev</a>
+        Development: <a href="https://joekarow.dev">JoeKarow.dev</a>
       </li>
       <li>
-        Design: <a href='http://html5up.net'>HTML5 UP</a>
+        Design: <a href="http://html5up.net">HTML5 UP</a>
       </li>
     </ul>
-  );
-};
+  )
+}
 
-export default Copyright;
+export default Copyright
