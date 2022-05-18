d1ec7b207b35f1996c12b4f34a22c978dea69332
Joe Karow <hello@joekarow.dev>
2022-05-18 17:01:57 -0400

    move breakpoint provider

--- gatsby-browser.jsx
+++ gatsby-browser.jsx
@@ -15,15 +15,14 @@ setDefaultBreakpoints([
   { lg: '981px' },
   { xl: '1281px' }
 ])
-
 export const wrapRootElement = ({ element }) => {
-  return (
-    <MDXProvider components={mdxShortcodes}>
-      <BreakpointProvider>{element}</BreakpointProvider>)
-    </MDXProvider>
-  )
+  return <MDXProvider components={mdxShortcodes}>{element}</MDXProvider>
 }
 
 export const wrapPageElement = ({ element, props }) => {
-  return <Layout {...props}>{element}</Layout>
+  return (
+    <Layout id="layoutWrap" {...props}>
+      <BreakpointProvider>{element}</BreakpointProvider>
+    </Layout>
+  )
 }
